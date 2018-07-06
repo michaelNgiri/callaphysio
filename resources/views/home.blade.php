2658958ea66e0938dce2785bdde16a664f6d38d7
alveis <michaelngiri@gmail.com>
2018-07-06 14:18:14 +0100

    show physios on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,21 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Call a Physio</div>
+                @forelse($closestPhysios as $closestPhysio)
+                    <div class="col-md-4">
+                        <div class="card-body">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
                         </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+                        <div class="card-footer">
+                            view direction on Map
+                        </div>
+                    </div>
+                @empty
+                    <p class="grey-text">No physio nearby</p>
+                @endforelse
             </div>
-        </div>
     </div>
 </div>
 @endsection
