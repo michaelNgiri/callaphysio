216c78e81dcd856b3e47fc1928633c4c773c46f6
alveis <michaelngiri@gmail.com>
2018-07-12 13:38:54 +0100

    checkpoint

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,10 @@
         </div>
     </div>
     <div class="row justify-content-center">
-              {{--<div class="card-header red-text"><b>Call a Physio</b></div>--}}
+
+                 <div class="col-md-12  red-text" style="background-color: #f3f3f3; padding: 1%;">
+                     <b>Call a Physio</b>
+                 </div>
               <div class="row">
                   @forelse($physios as $physio)
                       <div class="col-md-4">
@@ -31,13 +34,15 @@
                                       <div class="col-md-8 teal-text">
                                           <p>{{$physio->name()}}</p>
                                           <p><i class="mdi mdi-map-marker"></i>{{Str::words($physio->location(), 5,'....')}}</p>
+                                          <span><i class="mdi mdi-phone-classic"></i></span>
                                       </div>
                                   </div>
 
                               </div>
                               <div class="card-footer" style="font-size: 0.8em;">
-                                  <a style="color: #124641; float: left;" href="{{route('show-physio.map-view',['slug'=>$physio->slug])}}"><i class="mdi mdi-map-marker"></i>view Location on Map</a>
+                                  <a style="color: #124641; float: left;" href="{{route('show-physio.map-view',['slug'=>$physio->slug])}}"><i class="mdi mdi-google-maps"></i>view Location on Map</a>
                                   <a style="float:right; color: #ff7907;" href="{{route('show-physio.profile-view', ['slug'=>$physio->slug])}}"> View Profile</a>
+
                               </div>
                           </div>
                       </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,9 +26,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel red darken-4" style="color:#fff;">
             <div class="container">
-                {{--<a class="navbar-brand" href="{{ url('/') }}">--}}
-                    {{--{{ config('app.name', 'cap') }}--}}
-                {{--</a>--}}
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'cap') }}
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
