aa3a1bde73f33b0ec2ff3ee6af13dee864256cfd
alveis <michaelngiri@gmail.com>
2018-07-12 13:23:38 +0100

    resolve merge conflict

--- resources/views/physiotherapists/map-view.blade.php
+++ resources/views/physiotherapists/map-view.blade.php
@@ -1,10 +1,23 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">{{dd($physio)}}
-            <div class="map"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header red-text">
+                        Locate <b>{{$physio->name()}}</b> on Map
+                        <span style="float: right;">
+                        <img style=" border-radius: 20%;" height="48em" width="48em" src="{{asset($physio->imagePath())}}" alt="">
+                    </span>
+                    </div>
+                    <div class="card-body">
+                        <div class="map"></div>
+                    </div>
+                    <div class="card-footer">
+
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
 @endsection
