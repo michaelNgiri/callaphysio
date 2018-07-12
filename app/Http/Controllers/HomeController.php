73ea3ba989a471c39116043340104d0d25efb7f4
alveis <michaelngiri@gmail.com>
2018-07-12 11:26:49 +0100

    return cities to home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -27,6 +28,8 @@ class HomeController extends Controller
     {
         $closestPhysios = User::where('user_type', 2)->where('map_coordinates', Auth::user()->map_coordinates +- 300);
         is_null(Auth::user()->city)? $allPhysios = User::where('user_type', 2)->where('state', Auth::user()->state): $allPhysios = User::where('user_type', 2)->where('state', Auth::user()->city);
-        return view('home', compact('allPhysios', 'closestPhysios'));
+        $physios = User::paginate();
+        $cities = City::all();
+        return view('home', compact( 'physios','cities'));
     }
 }
