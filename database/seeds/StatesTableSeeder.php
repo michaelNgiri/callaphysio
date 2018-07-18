b6de20455103516a6863d10c52b84f7cc5d4043b
alveis <michaelngiri@gmail.com>
2018-07-18 11:49:50 +0100

    add all states + fct to seeder class

--- database/seeds/StatesTableSeeder.php
+++ database/seeds/StatesTableSeeder.php
@@ -12,18 +12,49 @@ class StatesTableSeeder extends Seeder
     public function run()
     {
        $states = [
-           'Abia',
-           'Adamawa',
-           "Akwa-Ibom",
-           'Anambra',
-           'Bauchi',
-           'Lagos',
-           'Kano',
-           'Abuja'
+                'Abia',
+                'Adamawa',
+                'Akwa Ibom',
+                'Anambra',
+                'Bauchi',
+                'Bayelsa',
+                'Benue',
+                'Borno',
+                'Cross River',
+                'Delta',
+                'Ebonyi',
+                'Enugu',
+                'Edo',
+                'Ekiti',
+                'Gombe',
+                'Imo',
+                'Jigawa',
+                'Kaduna',
+                'Kano',
+                'Katsina',
+                'Kebbi',
+                'Kogi',
+                'Kwara',
+                'Lagos',
+                'Nasarawa',
+                'Niger',
+                'Ogun',
+                'Ondo',
+                'Osun',
+                'Oyo',
+                'Plateau',
+                'Rivers',
+                'Sokoto',
+                'Taraba',
+                'Yobe',
+                'Zamfara',
+                'Abuja(Federal Capital Territory)'
        ];
 
        foreach ($states as $state){
-           \App\Models\State::create(['name'=>$state]);
+           \App\Models\State::create(['name'=>$state, 'country'=>1]);
         }
     }
 }
+
+
