7bda60ca08892f829ab30650c501b332f9218463
alveis <michaelngiri@gmail.com>
2018-07-19 11:51:42 +0100

    routes for locationing, add title to login and signup page

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,14 @@ Route::get('/home', 'CapController@index')->name('home');
 Route::get('/subscription', 'SubscriptionController@index');
 
 
+Route::group(['as'=>'home.'], function(){
+    Route::post('/home/filter/state', 'CapController@applyFIlter')->name('filter-by-state');
+});
+
+
 Route::group(['as'=>'show-physio.'], function(){
-   Route::get('{slug}/view/map-view', 'PhysiotherapistsController@mapView')->name('map-view');
-   Route::get('{slug}/view/profile-view', 'PhysiotherapistsController@profileView')->name('profile-view');
+    Route::get('{slug}/view/map-view', 'PhysiotherapistsController@mapView')->name('map-view');
+    Route::get('{slug}/view/profile-view', 'PhysiotherapistsController@profileView')->name('profile-view');
 });
 
 Route::group(['as'=>'consultations.'], function (){
